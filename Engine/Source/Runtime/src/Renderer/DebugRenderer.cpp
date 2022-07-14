d497536291a02799e91ebaa070f42e2e44846812
Batuhan Bozyel <batuhanbozyel@gmail.com>
2022-07-14 04:43:13 +0300

    [Engine][Renderer] Base debug renderer interface implementation

--- Engine/Source/Runtime/src/Renderer/DebugRenderer.cpp
+++ Engine/Source/Runtime/src/Renderer/DebugRenderer.cpp
@@ -5,8 +5,10 @@
 using namespace Gleam;
 
 DebugRenderer::DebugRenderer()
+	: mVertexBuffer(100)
 {
-    
+	mDebugProgram.vertexShader = ShaderLibrary::CreateShader("debugVertexShader", ShaderStage::Vertex);
+	mDebugProgram.fragmentShader = ShaderLibrary::CreateShader("debugFragmentShader", ShaderStage::Fragment);
 }
 
 DebugRenderer::~DebugRenderer()
@@ -16,5 +18,61 @@ DebugRenderer::~DebugRenderer()
 
 void DebugRenderer::Render()
 {
-    
+
+	// reset render queue
+	mLines.clear();
+	mDepthLines.clear();
+	mTrianlges.clear();
+	mDepthTrianlges.clear();
+}
+
+void DebugRenderer::DrawLine(const Vector3& start, const Vector3& end, Color32 color, bool depthTest)
+{
+	DebugLine line;
+	line.start = start;
+	line.end = end;
+	line.color = color;
+
+	if (depthTest)
+	{
+		mDepthLines.emplace_back(std::move(line));
+	}
+	else
+	{
+		mLines.emplace_back(std::move(line));
+	}
+}
+
+void DebugRenderer::DrawTriangle(const Vector3& v1, const Vector3& v2, const Vector3& v3, Color32 color, bool depthTest)
+{
+	DebugTriangle triangle;
+	triangle.vertex1 = v1;
+	triangle.vertex2 = v2;
+	triangle.vertex3 = v3;
+	triangle.color = color;
+
+	if (depthTest)
+	{
+		mDepthTrianlges.emplace_back(std::move(triangle));
+	}
+	else
+	{
+		mTrianlges.emplace_back(std::move(triangle));
+	}
+}
+
+void DebugRenderer::DrawQuad(const Vector3& center, float width, float height, Color32 color, bool depthTest)
+{
+	float halftWidth = width / 2.0f;
+	float halfHeight = height / 2.0f;
+
+	Vector3 v0{ center.x - halftWidth, center.y, center.z - halfHeight };
+	Vector3 v1{ center.x + halftWidth, center.y, center.z - halfHeight };
+	Vector3 v2{ center.x + halftWidth, center.y, center.z + halfHeight };
+	Vector3 v3{ center.x - halftWidth, center.y, center.z + halfHeight };
+
+	DrawLine(v0, v1, color, depthTest);
+	DrawLine(v1, v2, color, depthTest);
+	DrawLine(v2, v3, color, depthTest);
+	DrawLine(v3, v0, color, depthTest);
 }
--- Engine/Source/Runtime/src/Renderer/DebugRenderer.h
+++ Engine/Source/Runtime/src/Renderer/DebugRenderer.h
@@ -1,7 +1,26 @@
 #pragma once
+#include "Buffer.h"
+#include "Shader.h"
 #include "Renderer.h"
+#include "ShaderTypes.h"
+#include "CommandBuffer.h"
 
 namespace Gleam {
+
+struct DebugLine
+{
+	Vector3 start;
+	Vector3 end;
+	Color32 color;
+};
+
+struct DebugTriangle
+{
+	Vector3 vertex1;
+	Vector3 vertex2;
+	Vector3 vertex3;
+	Color32 color;
+};
 
 class DebugRenderer final : public Renderer
 {
@@ -12,7 +31,25 @@ public:
 	~DebugRenderer();
 
 	virtual void Render() override;
-    
+
+	void DrawLine(const Vector3& start, const Vector3& end, Color32 color, bool depthTest = true);
+
+	void DrawTriangle(const Vector3& v1, const Vector3& v2, const Vector3& v3, Color32 color, bool depthTest = true);
+
+	void DrawQuad(const Vector3& center, float width, float height, Color32 color, bool depthTest = true);
+
+private:
+
+	TArray<DebugLine> mLines;
+	TArray<DebugLine> mDepthLines;
+
+	TArray<DebugTriangle> mTrianlges;
+	TArray<DebugTriangle> mDepthTrianlges;
+
+	CommandBuffer mCommandBuffer;
+	GraphicsShader mDebugProgram;
+	VertexBuffer<DebugVertex> mVertexBuffer;
+
 };
 
 } // namespace Gleam
