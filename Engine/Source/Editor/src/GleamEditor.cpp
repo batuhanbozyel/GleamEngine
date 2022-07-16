9b5a047145f2d1acffe0a4b1ff5aed0818628c5c
Batuhan Bozyel <batuhanbozyel@gmail.com>
2022-07-16 22:20:14 +0300

    [Editor] Base editor scene grid added for testing

--- Engine/Source/Editor/src/GleamEditor.cpp
+++ Engine/Source/Editor/src/GleamEditor.cpp
@@ -1,26 +1,64 @@
+#define GDEBUG
 // EntryPoint
 #include "Core/EntryPoint.h"
+#include "Renderer/DebugRenderer.h"
 
 namespace GEditor {
 
-class GleamEditor : public Gleam::Application
+class SceneLayer : public Gleam::Layer
+{
+    virtual void OnAttach() override
+    {
+        Gleam::Camera camera(ApplicationInstance.GetActiveWindow().GetProperties().display.width,
+                             ApplicationInstance.GetActiveWindow().GetProperties().display.height);
+        camera.Translate({0.0f, -0.5f, -1.0f});
+        mRenderer.UpdateView(camera);
+    }
+    
+    virtual void OnUpdate() override
+    {
+        //mRenderer.DrawTriangle({-0.5f, -0.5f, 0.0f}, {0.5f, -0.5f, 0.0f}, {0.0f, 0.5f, 0.0f}, Gleam::Color::HSVToRGB(Gleam::Time::time, 1.0f, 1.0f));
+        
+        constexpr int gridWidth = 32;
+        constexpr int gridHeight = 32;
+        for (int i = 0; i < gridWidth; i++)
+        {
+            for (int j = 0; j < gridHeight; j++)
+            {
+                mRenderer.DrawQuad({float(i - gridWidth / 2), 0.0f, float(j - gridHeight / 2)}, 1.0f, 1.0f, Gleam::Color::HSVToRGB(Gleam::Time::time, 1.0f, 1.0f));
+            }
+        }
+    }
+    
+    virtual void OnRender() override
+    {
+        mRenderer.Render();
+    }
+
+private:
+    
+    Gleam::World mWorld;
+    Gleam::DebugRenderer mRenderer;
+};
+    
+class GleamEditor : public Gleam::Application, public Gleam::Layer
 {
 public:
 
 	GleamEditor(const Gleam::ApplicationProperties& props)
-		: Gleam::Application(props)
+		: Gleam::Application(props), mSceneLayer(Gleam::CreateRef<SceneLayer>())
 	{
-		
+        PushLayer(mSceneLayer);
 	}
 
 	~GleamEditor()
 	{
-		
+        RemoveLayer(mSceneLayer);
 	}
 
 private:
     
-    
+    Gleam::RefCounted<SceneLayer> mSceneLayer;
 
 };
 
