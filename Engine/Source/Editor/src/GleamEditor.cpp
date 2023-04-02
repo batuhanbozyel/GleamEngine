159d3fe464ec43209294c4375e8264a3667ba195
batuhanbozyel <batuhanbozyel@gmail.com>
2023-04-03 00:43:03 +0300

    [Editor] GleamEditor refactored to use new ViewStack system

--- Engine/Source/Editor/src/GleamEditor.cpp
+++ Engine/Source/Editor/src/GleamEditor.cpp
@@ -1,36 +1,38 @@
 #define GDEBUG
 // EntryPoint
 #include "Core/EntryPoint.h"
-#include "SceneView.h"
+#include "View/ViewStack.h"
+#include "View/SceneView/SceneView.h"
 
 namespace GEditor {
-    
-class GleamEditor : public Gleam::Application
+
+class GleamEditor : public Gleam::Game
 {
 public:
 
-	GleamEditor(const Gleam::ApplicationProperties& props)
-		: Gleam::Application(props)
+	GleamEditor(const Gleam::ApplicationProperties& properties)
+        : Gleam::Game(properties)
 	{
-        PushView<Gleam::SceneView>();
+        auto viewStack = GameInstance->AddSystem<ViewStack>();
+        viewStack->AddView<SceneView>();
 	}
 
 	~GleamEditor()
 	{
-        RemoveView<Gleam::SceneView>();
+        GameInstance->RemoveSystem<ViewStack>();
 	}
 
 private:
-
+    
 };
 
 } // namespace GEditor
 
-Gleam::Application* Gleam::CreateApplication()
+Gleam::Game* Gleam::CreateGameInstance()
 {
-	Gleam::ApplicationProperties props;
-	props.appVersion = Gleam::Version(1, 0, 0);
-	props.windowProps.title = "Gleam Editor";
-	props.windowProps.windowFlag = Gleam::WindowFlag::MaximizedWindow;
-	return new GEditor::GleamEditor(props);
+    Gleam::ApplicationProperties props;
+    props.appVersion = Gleam::Version(1, 0, 0);
+    props.windowProps.title = "Gleam Editor";
+    props.windowProps.windowFlag = Gleam::WindowFlag::MaximizedWindow;
+    return new GEditor::GleamEditor(props);
 }
