2b92a07a533d6f20c0d11b0d8fc401b0d67f37c7
batuhanbozyel <batuhanbozyel@gmail.com>
2023-05-01 19:35:44 +0300

    [Engine][Container] Added List and Queue containers

---
+++ Engine/Source/Runtime/src/Container/Queue.h
@@ -0,0 +1,16 @@
+//
+//  Queue.h
+//  GleamEngine
+//
+//  Created by Batuhan Bozyel on 29.04.2023.
+//
+
+#pragma once
+#include <queue>
+
+namespace Gleam {
+
+template<class T>
+using Queue = std::queue<T>;
+
+} // namespace Gleam
