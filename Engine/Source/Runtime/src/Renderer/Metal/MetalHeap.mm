54f4a94e60e906df90f068f59413c962bcc04cf8
batuhanbozyel <batuhanbozyel@gmail.com>
2023-10-13 13:44:23 +0300

    [Engine][Renderer] Metal: Heap type changed to placement

--- Engine/Source/Runtime/src/Renderer/Metal/MetalHeap.mm
+++ Engine/Source/Runtime/src/Renderer/Metal/MetalHeap.mm
@@ -7,6 +7,12 @@
 
 using namespace Gleam;
 
+static uint64_t AlignTo(const uint64_t offset, const uint64_t alignment)
+{
+    const bool isAligned = offset % alignment == 0;
+    return isAligned ? offset : ((offset / alignment) + 1) * alignment;
+}
+
 Heap::Heap(const HeapDescriptor& descriptor)
     : mDescriptor(descriptor)
 {
@@ -14,6 +20,7 @@ Heap::Heap(const HeapDescriptor& descriptor)
     MTLSizeAndAlign sizeAndAlign = [MetalDevice::GetHandle() heapBufferSizeAndAlignWithLength:descriptor.size options:resourceOptions];
     
     MTLHeapDescriptor* desc = [MTLHeapDescriptor new];
+    desc.type = MTLHeapTypePlacement;
     desc.resourceOptions = resourceOptions;
     desc.size = sizeAndAlign.size;
     mHandle = [MetalDevice::GetHandle() newHeapWithDescriptor:desc];
@@ -26,8 +33,11 @@ void Heap::Dispose()
 
 Buffer Heap::CreateBuffer(const BufferDescriptor& descriptor, size_t offset) const
 {
+    MTLResourceOptions resourceOptions = MemoryTypeToMTLResourceOption(mDescriptor.memoryType);
+    MTLSizeAndAlign sizeAndAlign = [MetalDevice::GetHandle() heapBufferSizeAndAlignWithLength:descriptor.size options:resourceOptions];
+    
     id<MTLHeap> heap = mHandle;
-    id<MTLBuffer> buffer = [heap newBufferWithLength:descriptor.size options:heap.resourceOptions];
+    id<MTLBuffer> buffer = [heap newBufferWithLength:descriptor.size options:heap.resourceOptions offset:AlignTo(offset, sizeAndAlign.align)];
 
     void* contents = nullptr;
     if (mDescriptor.memoryType != MemoryType::GPU)
