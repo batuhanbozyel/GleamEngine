f681692e09d373f9794665e2797b6d3f9d52199d
batuhanbozyel <batuhanbozyel@gmail.com>
2024-03-17 15:52:24 +0300

    [Engine][Renderer] Added simple phong lighting to mesh pass

--- Engine/Source/Runtime/src/Renderer/Shaders/ForwardPassShader.hlsl
+++ Engine/Source/Runtime/src/Renderer/Shaders/ForwardPassShader.hlsl
@@ -20,12 +20,17 @@ VertexOut forwardPassVertexShader(uint vertex_id: SV_VertexID)
 
     VertexOut OUT;
     OUT.position = mul(CameraBuffer.viewProjectionMatrix, mul(uniforms.modelMatrix, float4(position, 1.0f)));
-    OUT.normal = interleavedVert.normal.xyz;
+    OUT.normal = normalize(mul(uniforms.modelMatrix, float4(interleavedVert.normal, 0.0f)).xyz);
     OUT.texCoord = interleavedVert.texCoord;
     return OUT;
 }
 
 float4 forwardPassFragmentShader(VertexOut IN) : SV_TARGET
 {
-    return float4(1.0f, 0.71f, 0.75f, 1.0f);
+    float3 color = float3(IN.texCoord.x, IN.texCoord.y, 0.0f);
+    float3 ambient = color * 0.05f;
+    float3 lightDir = float3(0.43f, 0.43f, 0.0f);
+    float3 diffuse = color * max(dot(normalize(IN.normal), lightDir), 0.0f);
+    color = diffuse + ambient;
+    return float4(color.x, color.y, color.z, 1.0f);
 }
