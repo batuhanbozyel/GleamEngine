aeb8bdc45a8986e90512b86eb4738075f7cc5f17
batuhanbozyel <batuhanbozyel@gmail.com>
2025-05-15 11:05:42 +0300

    [Project][Reflection] GleamReflect (Clang/LLVM Reflection System) initial integration

--- Mac-GenerateProjects.sh
+++ Mac-GenerateProjects.sh
@@ -3,7 +3,7 @@ rm -rf build
 rm -rf bin
 mkdir build
 cd build
-cmake -G Xcode ..
+cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -G Xcode ..
 xattr -w com.apple.xcode.CreatedByBuildSystem true "$PWD/Debug"
 xattr -w com.apple.xcode.CreatedByBuildSystem true "$PWD/RelWithDebInfo"
 xattr -w com.apple.xcode.CreatedByBuildSystem true "$PWD/Release"
--- Win-GenerateProjects.bat
+++ Win-GenerateProjects.bat
@@ -4,4 +4,5 @@ IF EXIST bin rmdir /s /q bin
 mkdir build
 cd build
 cmake -G "Visual Studio 17 2022" ..
+powershell cpt -export-jsondb -proj Engine\Source\Runtime\Runtime.vcxproj
 PAUSE
