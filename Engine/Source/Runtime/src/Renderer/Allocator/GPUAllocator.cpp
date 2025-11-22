980891c10cc7a74532d90a16a928fcfdcec7187c
batuhanbozyel <batuhanbozyel@gmail.com>
2025-11-22 19:19:17 +0300

    [Engine][Renderer] VulkanMemoryAllocator integration

---
+++ Engine/Source/Runtime/src/Renderer/Allocator/GPUAllocator.cpp
@@ -0,0 +1,9 @@
+#include "gpch.h"
+#include "GPUAllocator.h"
+
+#define VMA_IMPLEMENTATION
+#define VMA_STATIC_VULKAN_FUNCTIONS 0
+#define VMA_DYNAMIC_VULKAN_FUNCTIONS 0
+#include <vk_mem_alloc.h>
+
+using namespace Gleam;
